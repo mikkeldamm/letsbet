638019d4b632acf7a58673dc243016c9e9bee151
mikkeldamm <mikkeldamm@hotmail.com>
2017-04-13 21:56:24 +0200

    Removed unused typing reference

--- references.d.ts
+++ references.d.ts
@@ -1,5 +1,3 @@
-/// <reference path="./node_modules/tns-core-modules/tns-core-modules.es6.d.ts" /> Needed for autocompletion and compilation.
-
 declare var UIKeyboardWillChangeFrameNotification: any;
 declare var UIKeyboardFrameEndUserInfoKey: any;
 
